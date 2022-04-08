3519ddf8430baa5e15a8eb9dd5c96aff6e499c9c
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-04-08 11:51:05 +0700

    delete file example

--- src/controller/example.controller.js
+++
@@ -1,9 +0,0 @@
-export const example = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    res.status(200).json({ message: `Test oke with ${id}` });
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-};
