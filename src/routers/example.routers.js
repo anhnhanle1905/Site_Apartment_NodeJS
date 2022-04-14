4692f1ee3d5578eb1a2583b442ab3a8ae762eb28
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-04-14 17:41:07 +0700

    Remove file example

--- src/routers/example.routers.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { example } from "../controller/example.controller.js";
-
-router.get("/:id", example);
-
-export default router;
